b6f5371db95f7660664662f6b19d42dce9484050
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-09-10 20:18:48 +0200

    Chart data mapped.

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -9,21 +9,29 @@ const Chart = ({country}) => {
 	const [monthData, updateData] = useState({
 		data: null,
 	});
-	const chartData = {
+	const [charData, updateChart] = useState({
 		dates: null,
 		dailyCases: null,
 		dailyDeaths: null
-	}
+	});
 	useEffect(() => {
 		const fetchMyData = async () => {
 			if(country){
 				const fetchedData = await fetchAllData(country);
 				updateData({data: fetchedData.data.dates});
-				loadChartData();
 			}
 		};
 		fetchMyData();
-	},[])
+	},[country])
+	useEffect(() => {
+		const dates = nestedLoop(monthData.data, 'date');
+		removeMatching(dates);
+		updateChart({
+			dates: dates,
+			dailyCases: nestedLoop(monthData.data, 'today_confirmed'),
+			dailyDeaths: nestedLoop(monthData.data, 'today_deaths')
+		})
+	},[monthData])
 
 	const nestedLoop = (obj, dataName) => {
 		const res = [];
@@ -36,7 +44,7 @@ const Chart = ({country}) => {
 					} else {
 						if(key == dataName){
 							res[res.length] = value;
-							//console.log(res);
+
 						}
 					}
 				}
@@ -55,31 +63,18 @@ const Chart = ({country}) => {
 		}
 		return arrayData;
 	}
-	const loadChartData = () => {
-		if(country !== null && country !== undefined){
-			// Test for mapping
-			const dates = nestedLoop(monthData.data, 'date');
-			removeMatching(dates);
-			console.log(dates);
-			chartData.dates = dates;
-			chartData.dailyCases = nestedLoop(monthData.data, 'today_confirmed');
-			chartData.dailyDeaths = nestedLoop(monthData.data, 'today_deaths');
-			console.log(chartData);
-		}
-	}
-	//console.log(chartData);
 	const lineChart = (
-		chartData ? (
+		charData ? (
 		  <Line
 			data={{
-			  labels: Object.keys(chartData).map(({ dates }) => new Date(dates).toLocaleDateString()),
+			  labels: Object.values(charData.dates).map((dates) => dates),
 			  datasets: [{
-				data: Object.keys(chartData).map((dailyCases) => dailyCases),
+				data: Object.values(charData.dailyCases).map((dailyCases) => dailyCases),
 				label: 'Cases',
 				borderColor: '#3333ff',
 				fill: true,
 			  }, {
-				data: Object.keys(chartData).map((dailyDeaths) => dailyDeaths),
+				data: Object.values(charData.dailyDeaths).map((dailyDeaths) => dailyDeaths),
 				label: 'Deaths',
 				borderColor: 'red',
 				backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -90,7 +85,6 @@ const Chart = ({country}) => {
 		  />
 		) : null
 	);
-	console.log(chartData);
 
 
     return(
